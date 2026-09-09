62418cf5f178789122d2c99c8b82256e18326d8a
dbm1982 <dbm1982@Hotmail.com>
2026-09-08 20:31:02 -0400

    Update Weekly Visual Rec Schedule.py

--- Weekly Visual Rec Schedule.py
+++ Weekly Visual Rec Schedule.py
@@ -104,7 +104,7 @@ def field_sort_key(field_label):
 # --- Auto Color Detection ---
 def get_color_map_from_schedule(future_games):
     known_colors = {
-        "Blue": "#4996D1", "Red": "#E88989", "Green": "#429964",
+        "Blue": "#4996D1", "Red": "#dc4949", "Green": "#429964",
         "Orange": "#FCB03A", "Berry": "#E8DAEF", "Gray": "#F2F3F4",
         "Black": "#000000", "White": "#FFFFFF", "Yellow": "#FFEB3B",
         "Purple": "#9B59B6", "Maroon": "#800000", "Teal": "#008080",

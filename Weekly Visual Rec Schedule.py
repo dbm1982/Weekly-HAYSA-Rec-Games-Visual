68552a5cc631deabdb0a5e0288d415c894437674
dbm1982 <dbm1982@Hotmail.com>
2026-09-16 22:37:46 -0400

    Update Weekly Visual Rec Schedule.py

--- Weekly Visual Rec Schedule.py
+++ Weekly Visual Rec Schedule.py
@@ -17,7 +17,7 @@ today = datetime.now(local_tz).date()
 travel_towns = {
     "Stoughton", "Sharon", "Raynham", "Bridgewater", "Mansfield",
     "Canton", "Foxboro", "Easton", "Taunton", "Whitman", "Abington",
-    "Quincy"
+    "Quincy", "Weymouth"
 }
 
 # --- Fallback colors for no-color teams ---
